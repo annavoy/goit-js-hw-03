70810acb1c20609935d92f802743eb2d596a81b0
Anna <36787402+annavoy@users.noreply.github.com>
2024-11-18 16:31:19 +0000

    update js files

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,16 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
+function filterArray(numbers, value) {
+  let newArray = [];
 
-  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
+  }
 
-  return totalWidth;
+  return newArray;
 }
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
